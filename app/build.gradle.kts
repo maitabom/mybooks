ac5cc74971f56cea398d9786cdd187600a091af9
Fábio Valentim <maitabom@gmail.com>
2025-11-16 18:02:30 -0300

    feat: Migra dependências de navegação para o `libs`
    Muda as dependências `navigation-fragment-ktx` e `navigation-ui-ktx` para utilizar a versão centralizada definida no catálogo de versões (`libs`).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,14 @@ android {
 }
 
 dependencies {
-
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.6")
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
