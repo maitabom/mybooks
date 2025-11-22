f70f7f5046edf8c29802208d6c268139b4e86e0e
Fábio Valentim <maitabom@gmail.com>
2025-11-22 14:43:31 -0300

    feat: Configura RecyclerView para lista de livros
    Configura o `RecyclerView` na `HomeFragment` com um `LinearLayoutManager` e um `BookAdapter`.
    Isso prepara a tela inicial para exibir uma lista de livros.

--- app/src/main/java/net/baudovalentim/mybooks/ui/fragments/HomeFragment.kt
+++ app/src/main/java/net/baudovalentim/mybooks/ui/fragments/HomeFragment.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import net.baudovalentim.mybooks.databinding.FragmentHomeBinding
+import net.baudovalentim.mybooks.ui.adapter.BookAdapter
 import net.baudovalentim.mybooks.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val homeViewModel: HomeViewModel by viewModels()
+    private val adapter: BookAdapter = BookAdapter()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,8 +26,10 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding.rvBooks.layoutManager = LinearLayoutManager(context)
+        binding.rvBooks.adapter = adapter
 
+        val root: View = binding.root
         return root
     }
 
