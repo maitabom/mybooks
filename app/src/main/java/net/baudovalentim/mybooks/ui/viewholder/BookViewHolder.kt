836ecda67c812745d17ceed53bd0028924d931af
Fábio Valentim <maitabom@gmail.com>
2025-11-22 15:52:31 -0300

    feat: Implementa lógica de exibição no `BookViewHolder`
    Aplica a lógica para customizar a aparência do `BookViewHolder` com base nos dados do livro.
    
    - Adiciona um `when` para definir o plano de fundo da `TextView` de gênero (`tvGenero`) de acordo com o gênero do livro, utilizando os drawables `rounded_label_red`, `rounded_label_fantasy` ou `rounded_label_teal`.
    - Implementa o método `updateFavoriteIcon` para alterar o ícone de favorito (`ivFavorite`) entre `ic_favorite` e `ic_favorite_empty` com base no status de favorito do livro.

--- app/src/main/java/net/baudovalentim/mybooks/ui/viewholder/BookViewHolder.kt
+++ app/src/main/java/net/baudovalentim/mybooks/ui/viewholder/BookViewHolder.kt
@@ -1,6 +1,7 @@
 package net.baudovalentim.mybooks.ui.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import net.baudovalentim.mybooks.R
 import net.baudovalentim.mybooks.databinding.ItemBookBinding
 import net.baudovalentim.mybooks.entity.BookEntity
 
@@ -10,5 +11,17 @@ class BookViewHolder(private val item: ItemBookBinding): RecyclerView.ViewHolder
         item.tvAuthor.text = book.author
         item.tvGenero.text = book.genre
 
+        when (book.genre) {
+            "Terror" -> item.tvGenero.setBackgroundResource(R.drawable.rounded_label_red)
+            "Fantasia" -> item.tvGenero.setBackgroundResource(R.drawable.rounded_label_fantasy)
+            else -> item.tvGenero.setBackgroundResource(R.drawable.rounded_label_teal)
+        }
+
+        updateFavoriteIcon(book.favorite)
+    }
+
+    private fun updateFavoriteIcon(favorite: Boolean) {
+        val icon = if (favorite) R.drawable.ic_favorite else R.drawable.ic_favorite_empty
+        item.ivFavorite.setImageResource(icon)
     }
 }
