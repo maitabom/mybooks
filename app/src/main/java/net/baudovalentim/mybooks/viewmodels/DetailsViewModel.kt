e3a532c2d6dc85526da8ae5cbb4fab69504bfecf
Fábio Valentim <maitabom@gmail.com>
2025-11-23 15:21:51 -0300

    feat: Implementa ViewModel para detalhes do livro
    Implementa a lógica no `DetailsViewModel` para gerenciar os dados da tela de detalhes.
    
    Adiciona a `BookRepository` como dependência para buscar informações do livro.
    Expõe um `LiveData` para observar um `BookEntity`.
    Cria o método `getBookById` para carregar um livro específico a partir do repositório.

--- app/src/main/java/net/baudovalentim/mybooks/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/net/baudovalentim/mybooks/viewmodels/DetailsViewModel.kt
@@ -1,7 +1,17 @@
 package net.baudovalentim.mybooks.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.baudovalentim.mybooks.entity.BookEntity
+import net.baudovalentim.mybooks.repository.BookRepository
 
 class DetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val repository: BookRepository = BookRepository()
+    private val _book = MutableLiveData<BookEntity>()
+    val book: LiveData<BookEntity> = _book
+
+    fun getBookById(bookId: Int) {
+        _book.value = repository.getBookById(bookId)
+    }
 }
