2fff74d3c7f1221f8d834c04559fa0ec79804a38
Fábio Valentim <maitabom@gmail.com>
2025-11-23 15:49:29 -0300

    feat: Adiciona funcionalidade de exclusão e favoritar no DetailsViewModel
    Adiciona as seguintes funcionalidades ao `DetailsViewModel`:
    - `deleteBook(bookId: Int)`: Método para excluir um livro, atualizando o `LiveData` `bookRemoval` para notificar a UI sobre o sucesso da operação.
    - `toggleFavorite(bookId: Int)`: Método para alternar o estado de favorito de um livro.

--- app/src/main/java/net/baudovalentim/mybooks/viewmodels/DetailsViewModel.kt
+++ app/src/main/java/net/baudovalentim/mybooks/viewmodels/DetailsViewModel.kt
@@ -9,9 +9,19 @@ import net.baudovalentim.mybooks.repository.BookRepository
 class DetailsViewModel : ViewModel() {
     private val repository: BookRepository = BookRepository()
     private val _book = MutableLiveData<BookEntity>()
+    private val _bookRemoval = MutableLiveData<Boolean>()
     val book: LiveData<BookEntity> = _book
+    val bookRemoval: LiveData<Boolean> = _bookRemoval
 
     fun getBookById(bookId: Int) {
         _book.value = repository.getBookById(bookId)
+    }
+
+    fun deleteBook(bookId: Int) {
+        _bookRemoval.value = repository.delete(bookId)
+    }
+
+    fun toggleFavorite(bookId: Int) {
+        repository.toggleFavorite(bookId)
     }
 }
