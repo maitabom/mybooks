353b9564b2bdd9dad93e5399883de7ac53c8911d
Fábio Valentim <maitabom@gmail.com>
2025-11-23 15:49:20 -0300

    feat: Adiciona interações de exclusão e favoritar livro
    Implementa a funcionalidade de exclusão de um livro, que exibe um diálogo de confirmação (`AlertDialog`) antes de remover o item. Após a exclusão, uma mensagem de sucesso é exibida e o usuário é redirecionado para a tela anterior.
    
    Adiciona também a lógica para favoritar e desfavoritar um livro ao interagir com o `CheckBox` correspondente.

--- app/src/main/java/net/baudovalentim/mybooks/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/net/baudovalentim/mybooks/ui/fragments/DetailsFragment.kt
@@ -1,9 +1,12 @@
 package net.baudovalentim.mybooks.ui.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import net.baudovalentim.mybooks.R
@@ -52,9 +55,40 @@ class DetailsFragment : Fragment() {
                 else -> binding.tvGenreValue.setBackgroundResource(R.drawable.rounded_label_teal)
             }
         }
+
+        detailsdViewModel.bookRemoval.observe(viewLifecycleOwner) {
+            if (it) {
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.msg_removed_successfully),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
     }
 
     private fun setListeners() {
         binding.ivBack.setOnClickListener { requireActivity().supportFragmentManager.popBackStack() }
+        binding.btRemove.setOnClickListener { handleRemove() }
+        binding.cbFavorite.setOnClickListener { handleFavorite() }
+    }
+
+    private fun handleFavorite() {
+        detailsdViewModel.toggleFavorite(bookId)
+    }
+
+    private fun handleRemove() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setMessage(R.string.dialog_message_delete_item)
+            .setPositiveButton(
+                R.string.dialog_positive_button_yes
+            ) { dialog, which -> detailsdViewModel.deleteBook(bookId) }
+            .setNegativeButton(
+                R.string.dialog_negative_button_no
+            ) { dialog, which -> dialog.dismiss() }
+
+        builder.create().show()
     }
 }
